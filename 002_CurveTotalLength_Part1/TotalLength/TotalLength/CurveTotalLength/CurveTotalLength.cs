7195e03a99171e043a6b9b8e8b8f6990a0850f3f
Konrad K Sobon <ksobon1986@gmail.com>
2016-12-11 18:46:53 -0500

    clean up

---
+++ 002_CurveTotalLength_Part1/TotalLength/TotalLength/CurveTotalLength/CurveTotalLength.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI.Selection;
+
+namespace TotalLength
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CurveTotalLength : IExternalCommand
+    {
+        public class DetailLineFilter : ISelectionFilter
+        {
+            public bool AllowElement(Element e)
+            {
+                return (e.Category.Id.IntegerValue.Equals(
+                  (int)BuiltInCategory.OST_Lines));
+            }
+            public bool AllowReference(Reference r, XYZ p)
+            {
+                return false;
+            }
+        }
+
+        public Result Execute(
+            ExternalCommandData commandData, 
+            ref string message, 
+            ElementSet elements)
+        {
+            // Get application and document objects
+            UIApplication uiApp = commandData.Application;
+
+            try
+            {
+                // Implement Selection Filter to select curves
+                IList<Element> pickedRef = null;
+                Selection sel = uiApp.ActiveUIDocument.Selection;
+                DetailLineFilter selFilter = new DetailLineFilter();
+                pickedRef = sel.PickElementsByRectangle(selFilter, "Select lines");
+
+                // Measure their total length
+                List<double> lengthList = new List<double>();
+                foreach (Element e in pickedRef)
+                {
+                    DetailLine line = e as DetailLine;
+                    if (line != null)
+                    {
+                        lengthList.Add(line.GeometryCurve.Length);
+                    }
+                }
+
+                string lengthFeet = Math.Round(lengthList.Sum(), 2).ToString() + " ft";
+                string lengthMeters = Math.Round(lengthList.Sum() * 0.3048, 2).ToString() + " m";
+                string lengthMilimeters = Math.Round(lengthList.Sum() * 304.8, 2).ToString() + " mm";
+                string lengthInch = Math.Round(lengthList.Sum() * 12, 2).ToString() + " inch";
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("Total Length is:");
+                sb.AppendLine(lengthFeet);
+                sb.AppendLine(lengthInch);
+                sb.AppendLine(lengthMeters);
+                sb.AppendLine(lengthMilimeters);
+                
+                // Return a message window that displays total length to user
+                TaskDialog.Show("Line Length", sb.ToString());
+
+                // Assuming that everything went right return Result.Succeeded
+                return Result.Succeeded;
+            }
+            // This is where we "catch" potential errors and define how to deal with them
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                // If user decided to cancel the operation return Result.Canceled
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                // If something went wrong return Result.Failed
+                message = ex.Message;
+                return Result.Failed;
+            }  
+        }
+    }
+}
---
+++ 003_GrimshawRibbon/GrimshawRibbon/GrimshawRibbon/App.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows.Media.Imaging;
+
+namespace GrimshawRibbon
+{
+    class App : IExternalApplication
+    {
+        // define a method that will create our tab and button
+        static void AddRibbonPanel(UIControlledApplication application)
+        {
+            // Create a custom ribbon tab
+            String tabName = "Grimshaw";
+            application.CreateRibbonTab(tabName);
+
+            // Add a new ribbon panel
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "Tools");
+
+            // Get dll assembly path
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            // create push button for CurveTotalLength
+            PushButtonData b1Data = new PushButtonData(
+                "cmdCurveTotalLength", 
+                "Total" + System.Environment.NewLine + "  Length  ", 
+                thisAssemblyPath,
+                "TotalLength.CurveTotalLength");
+
+            PushButton pb1 = ribbonPanel.AddItem(b1Data) as PushButton;
+            pb1.ToolTip = "Select Multiple Lines to Obtain Total Length";
+            BitmapImage pb1Image = new BitmapImage(new Uri("pack://application:,,,/GrimshawRibbon;component/Resources/totalLength.png"));
+            pb1.LargeImage = pb1Image;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // do nothing
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // call our method that will load up our toolbar
+            AddRibbonPanel(application);
+            return Result.Succeeded;
+        }
+    }
+}
